c54a1683e93d27db90c4baaec4c6ed4492e8a097
Unknown <michael.white+dev@10dot1.co.uk>
2018-03-17 14:31:14 +0000

    cors

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ exports.handler = (event, context, callback) => {
         {
             "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
             "Access-Control-Allow-Methods": "GET,OPTIONS",
-            "Access-Control-Allow-Origin": originURL
+            "Access-Control-Allow-Origin": "*"
         }
     });
 }
